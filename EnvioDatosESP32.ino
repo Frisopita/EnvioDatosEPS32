55ba362324c4cee0b40e4af7c22f53a9b03c3d70
Frida Sofía Andrade Sierra <frisoandrade@gmail.com>
2023-06-28 11:17:10 -0600

    last version compling

--- EnvioDatosESP32.ino
+++ EnvioDatosESP32.ino
@@ -6,7 +6,6 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-<<<<<<< HEAD
 
 #define SERIAL_FRAME_SYNC1 0
 #define SERIAL_FRAME_SYNC2 1
@@ -15,16 +14,11 @@
 #define SERIAL_FRAME_CRC 4
 #define SERIAL_FRAME_LENGHT 5
 
-#define ID_HEART_RATE 0
-#define ID_SPO 1
-
 #define MAX_DATA_SIZE 12
 uint8_t dataArray[MAX_DATA_SIZE];
 uint8_t dataSize = 0;
 uint8_t txArray[SERIAL_FRAME_LENGHT];
 
-=======
->>>>>>> ca38aeaf92b369bd2efe433948ad1b2e4e5eea89
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_S1_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -39,8 +33,6 @@ uint8_t txArray[SERIAL_FRAME_LENGHT];
 #define CHARACTERISTIC_S10_UUID "a8f2dbc3-c562-42d9-a094-33e4cca73118"
 #define CHARACTERISTIC_S11_UUID "3c21b038-85a3-4c47-aa78-446f301dd61c"
 #define CHARACTERISTIC_S12_UUID "1b0724f2-156b-41a6-8bb6-22be491731fc"
-
-
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristicS1 = NULL;
 BLECharacteristic* pCharacteristicS2 = NULL;
@@ -57,7 +49,6 @@ BLECharacteristic* pCharacteristicS12 = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
-
 int S1 = 120;
 int S2 = 100;
 int S3 = 40;
@@ -70,17 +61,49 @@ int S9 = 50;
 int S10 = 65;
 int S11 = 75;
 int S12 = 85;
-
-struct SensorData {
-  uint32_t memoryAddress;
-  uint32_t sensorId;
-  float sensorData;
-};
-SensorData dataArr[10];  // Tamaño del arreglo: 10 sensores (puedes ajustarlo según tus necesidades)
-
-
-
 static BLERemoteCharacteristic* pRemoteCharacteristicRea;
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+class MyCallbacks: public BLECharacteristicCallbacks{
+  void onWrite(BLECharacteristic *pCharacteristic){
+    std::string rxValue = pCharacteristic->getValue();
+    BLEUUID RxUuid = pCharacteristic->getUUID();
+
+     if (rxValue.length() > 0) {
+    // Limpiar el arreglo antes de copiar los nuevos datos
+    memset(dataArray, 0, sizeof(dataArray));
+    // Copiar los datos recibidos en el arreglo
+    dataSize = rxValue.length();
+    for (int i = 0; i < dataSize; i++) {
+      dataArray[i] = rxValue[i];
+    }
+    Serial.println("Recibiendo datos");
+    Serial.print("Datos: ");
+    for (int i = 0; i < dataSize; i++) {
+      Serial.print(dataArray[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+    Serial.println("Finalizar recepción");
+  }
+  }
+};
+
+void printDataArray() {
+  Serial.print("Data Array: ");
+  for (int i = 0; i < dataSize; i++) {
+    Serial.print(dataArray[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
 
 void encodeMessage(int id, int value){
   txArray[SERIAL_FRAME_SYNC1] = 0xA5;
@@ -89,143 +112,36 @@ void encodeMessage(int id, int value){
   txArray[SERIAL_FRAME_VALUE] = value;
   txArray[SERIAL_FRAME_CRC] = 0;
 }
-void sendMessage()
-{
-    for (int i = 0; i < SERIAL_FRAME_LENGHT; i++) {
-      Serial.print(txArray[i]);
-    }
-    Serial.println();
-}
-
-
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      BLEDevice::startAdvertising();
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
-};
-
-
-class MyCallbacks: public BLECharacteristicCallbacks{
-  void onWrite(BLECharacteristic *pCharacteristic){
-    std::string rxValue = pCharacteristic->getValue();
-    BLEUUID RxUuid = pCharacteristic->getUUID();
-
-<<<<<<< HEAD
-     if (rxValue.length() > 0) {
-    // Limpiar el arreglo antes de copiar los nuevos datos
-    memset(dataArray, 0, sizeof(dataArray));
-
-    // Copiar los datos recibidos en el arreglo
-    dataSize = rxValue.length();
-    for (int i = 0; i < dataSize; i++) {
-      dataArray[i] = rxValue[i];
-    }
-
-    if(1)//RxUuid.equals(CHARACTERISTIC_S1_UUID))
-    {
-      Serial.print("Hola");
-      encodeMessage(ID_HEART_RATE, dataArray[4]);
-    }
-    else if(0)//RxUuid.equals(CHARACTERISTIC_S2_UUID))
-    {
-      encodeMessage(ID_SPO, dataArray[4]);
-    }
-    else
-    {
-
-    }
-    sendMessage();
-
-   
-=======
-  if (rxValue.length() > 0) {
-    Serial.println("Recibiendo datos");
-    Serial.print("Datos:");
-
-    for (int i = 0; i < rxValue.length(); i++) {
-      Serial.print(rxValue[i]);
-    }
-
-    // Deserializar los datos y almacenarlos en el arreglo
-    if (rxValue.length() == sizeof(SensorData)) {
-      SensorData receivedData;
-      memcpy(&receivedData, rxValue.data(), sizeof(SensorData));
-
-      // Guardar los datos en el arreglo
-      dataArr[receivedData.sensorId] = receivedData;
-
-      // Acceder a los datos individualmente
-      Serial.print("Memory Address: ");
-      Serial.println(dataArr[receivedData.sensorId].memoryAddress);
-      Serial.print("Sensor ID: ");
-      Serial.println(dataArr[receivedData.sensorId].sensorId);
-      Serial.print("Sensor Data: ");
-      Serial.println(dataArr[receivedData.sensorId].sensorData);
-    }
-
-    Serial.println();
-    Serial.println("Finalizar recepción");
->>>>>>> ca38aeaf92b369bd2efe433948ad1b2e4e5eea89
-  }
-  }
-};
 
 // Thread  
 #include <Thread.h>
 #include <ThreadController.h>
-
 // ThreadController that will controll all threads
 ThreadController controll = ThreadController();
-
 Thread* btThread = new Thread();
-
 
 void setup() {
   Serial.begin(115200);
   initBT();
-  
+
   btThread->onRun(btCallback);
   btThread->setInterval(100);
-
   controll.add(btThread);
 }
-
 void loop() {
 // put your main code here, to run repeatedly: 
   controll.run();
-
      //Aumentar los valores de los sensores
      // S1 = S1+1;
      // S2 = S2+1;
-      
+
       delay(2000);
-
-
-  
 }
 
 void btCallback(){
   // notify changed value
     if (deviceConnected) {
-
-      byte serializedData[sizeof(SensorData)];
-
-    // Iterar sobre el arreglo de datos y enviar cada elemento
-    for (int i = 0; i < 10; i++) {  // Cambia el límite según el tamaño de tu arreglo
-      // Serializar los datos
-      memcpy(serializedData, &dataArr[i], sizeof(SensorData));
-
-      // Enviar los datos serializados a través de Bluetooth
-      pCharacteristicS1->setValue(serializedData, sizeof(SensorData));
-      pCharacteristicS1->notify();
-      // Repite los pasos anteriores para cada característica (pCharacteristicS2, pCharacteristicS3, etc.) según sea necesario
-    }
-
+     
       String strS1 = "";
       strS1 += S1;
       String strS2 = "";
@@ -250,42 +166,31 @@ void btCallback(){
       strS11 += S11;
       String strS12 = "";
       strS12 += S12;
-      
-      
-      
+
+
+
       pCharacteristicS1->setValue((char*)strS1.c_str());
       pCharacteristicS1->notify();
-
       pCharacteristicS2->setValue((char*)strS2.c_str());
       pCharacteristicS2->notify();
-
       pCharacteristicS3->setValue((char*)strS3.c_str());
       pCharacteristicS3->notify();
-
       pCharacteristicS4->setValue((char*)strS4.c_str());
       pCharacteristicS4->notify();
-
       pCharacteristicS5->setValue((char*)strS5.c_str());
       pCharacteristicS5->notify();
-
       pCharacteristicS6->setValue((char*)strS6.c_str());
       pCharacteristicS6->notify();
-
       pCharacteristicS7->setValue((char*)strS7.c_str());
       pCharacteristicS7->notify();
-
       pCharacteristicS8->setValue((char*)strS8.c_str());
       pCharacteristicS8->notify();
-
       pCharacteristicS9->setValue((char*)strS9.c_str());
       pCharacteristicS9->notify();
-
       pCharacteristicS10->setValue((char*)strS10.c_str());
       pCharacteristicS10->notify();
-
       pCharacteristicS11->setValue((char*)strS11.c_str());
       pCharacteristicS11->notify();
-
       pCharacteristicS12->setValue((char*)strS12.c_str());
       pCharacteristicS12->notify();
     }
@@ -303,30 +208,15 @@ void btCallback(){
     }
 }
 
-<<<<<<< HEAD
-void printDataArray() {
-  Serial.print("Data Array: ");
-  for (int i = 0; i < dataSize; i++) {
-    Serial.print(dataArray[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
-}
-
-=======
->>>>>>> ca38aeaf92b369bd2efe433948ad1b2e4e5eea89
 
 void initBT(){
   // Create the BLE Device
   BLEDevice::init("ESP32 Sopita");
-
   // Create the BLE Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
   // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
-
   // Create a BLE Characteristic
   pCharacteristicS1 = pService->createCharacteristic(
                       CHARACTERISTIC_S1_UUID,
@@ -452,7 +342,6 @@ void initBT(){
   pCharacteristicS12->addDescriptor(new BLE2902());
   // Start the service
   pService->start();
-
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
